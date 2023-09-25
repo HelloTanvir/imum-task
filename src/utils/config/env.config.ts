88e6d1dfaeae61145b16a702a19475d59df1cde5
HelloTanvir <tanvirhossain.butex@gmail.com>
2023-09-25 19:39:47 +0600

    moved based url from env to config file

--- src/utils/config/env.config.ts
+++ src/utils/config/env.config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const envConfig = {
     port: process.env.PORT || 5000,
-    baseUrl: process.env.BASE_URL,
+    baseUrl: 'https://www.otomoto.pl',
     initialUrl:
         '/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc',
 };
